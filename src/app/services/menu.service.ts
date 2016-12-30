79484928621751a3868b0195a139a988ddaa377a
Arnold Krumins <arnold.krumins@gmail.com>
2016-12-30 21:37:09 +0100

    adding menu service

---
+++ src/app/services/menu.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MenuService {
+
+  constructor() { }
+
+  getMenuItems(){
+
+    return [
+      { 'name':'DASHBOARD', submenus:[]},
+      { 'name':'DEMAND', submenus:[{'name':'Bids',route:''},{'name':'Credit',route:''}]},
+      { 'name':'ORDERS', submenus:[{'name':'Marketplace',route:''},{'name':'Direct',route:''},{'name':'Digital',route:''},{'name':'OTT',route:''}]},
+      { 'name':'SETUP', submenus:[{'name':'Users',route:''},{'name':'Orders',route:''},{'name':'Channels',route:''},{'name':'Bids',route:''}]}
+    ];
+
+}
+
+}
