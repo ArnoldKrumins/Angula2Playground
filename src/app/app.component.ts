1856f0522ea995123e18b59b64bd429e778fe8c1
Arnold Krumins <arnold.krumins@gmail.com>
2016-12-30 21:35:58 +0100

    adding menu service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {MenuService} from "./services/menu.service";
 
 @Component({
   selector: 'app-root',
+  providers:[MenuService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  private items;
+
+  constructor(private menuService:MenuService){
+     this.items = menuService.getMenuItems();
+  }
 
 }
