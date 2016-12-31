0aff9425cf5bcd4a6ada4a425a5ebcd507a0a935
Arnold Krumins <arnold.krumins@gmail.com>
2016-12-31 15:06:55 +0100

    adding items to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+///<reference path="../../node_modules/@angular/core/src/metadata/lifecycle_hooks.d.ts"/>
+import {Component, OnInit} from '@angular/core';
 import {MenuService} from "./services/menu.service";
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   providers:[MenuService],
+  directives:[NgFor],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  private items;
+  private items:any;
 
   constructor(private menuService:MenuService){
-     this.items = menuService.getMenuItems();
+
+  }
+
+  ngOnInit(){
+    this.items = this.menuService.getMenuItems();
+    console.log(this.items);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
+
+
   ],
   imports: [
     BrowserModule,
