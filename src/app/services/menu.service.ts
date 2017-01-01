69e83af18327c52a43e2ad51bf7b3f41b00dba2d
Arnold Krumins <arnold.krumins@gmail.com>
2017-01-01 14:05:32 +0100

    more small changes

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -8,7 +8,7 @@ export class MenuService {
   getMenuItems(){
 
     return [
-      { 'name':'DASHBOARD', submenus:[]},
+      { 'name':'DASHBOARD', submenus:[{'name':'Bids',route:''},{'name':'Credit',route:''}]},
       { 'name':'DEMAND', submenus:[{'name':'Bids',route:''},{'name':'Credit',route:''}]},
       { 'name':'ORDERS', submenus:[{'name':'Marketplace',route:''},{'name':'Direct',route:''},{'name':'Digital',route:''},{'name':'OTT',route:''}]},
       { 'name':'SETUP', submenus:[{'name':'Users',route:''},{'name':'Orders',route:''},{'name':'Channels',route:''},{'name':'Bids',route:''}]}
